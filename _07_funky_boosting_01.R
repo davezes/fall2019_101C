c85ae76c8b77d982f6492139b4d50fa3569367ed
Dave Zes <davezes@Daves-Mac-Pro.local>
2019-11-26 09:00:21 -0800

    adds

--- _07_funky_boosting_01.R
+++ _07_funky_boosting_01.R
@@ -82,7 +82,11 @@ xrmse <- sqrt( mean( (y0 - y0hat)^2 ) ) ; xrmse
 
 
 
-
+if(!dir.exists(file.path("~", "Desktop", "anim02"))) {
+    dir.create(file.path("~", "Desktop", "anim02"))
+} else {
+    unlink(file.path("~", "Desktop", "anim02", "*"))
+}
 
 
 ## f_best_split(x=x1, y=y, rtries=10, mincellsize=5)
@@ -114,6 +118,29 @@ for(iib in 1:B) {
     
     cat(mean(rr^2), "\n")
     
+    if(TRUE) {
+        if(xbool.savePlots) { png(file.path("~", "Desktop", "anim02", paste0("frame_", sprintf("%06d", iib), ".png")), width=800, height=800, pointsize=28) }
+        par(mfrow=c(1,1), mar=c(1,1,1,1), bg="#000000")
+        yhat_unit <- (ftreeq + 1.1) / 2.2
+        xxcols <- hcl.colors(301, "YlOrRd", rev = FALSE)[ round(yhat_unit * 300)+1 ]
+        plot(x2,  x1, col = xxcols, xlab="x2", ylab="x1", pch=19, main=iib, col.main="#FFFFFF", cex=0.7)
+        if(xbool.savePlots) { dev.off() }
+    }
+    
+}
+
+
+if(xbool.savePlots) {
+    xxsysstr <-
+    paste0(
+    "ffmpeg  -y  -loglevel error -r 30   " ,
+    "  -i  ",
+    file.path("~", "Desktop", "anim02", paste0("frame_", "%06d.png")),
+    "  -codec:v libx264   -pix_fmt yuv420p  -r 15  -preset slow  ",
+    file.path("~", "Desktop", "mov_boost_01.mp4")
+    )
+    
+    system(xxsysstr)
 }
 
 
@@ -140,11 +167,15 @@ if(xbool.savePlots) { dev.off() }
 
 
 
-####################### interacted
+####################### interacted natural splines
 
+
+xknots <- seq(-4, 4, by=1)
+
+mxH1 <- f_basisNatSplines(x=x1, xknots=xknots)
+mxH2 <- f_basisNatSplines(x=x2, xknots=xknots)
 
 xxeg <- expand.grid( "ndx1"=I(1:ncol(mxH1)), "ndx2"=I(1:(ncol(mxH1)-1)) )
-
 
 mxX <- mxH1[ , xxeg[ , "ndx1"] ] * mxH2[ , -1][ , xxeg[ , "ndx2"] ]
 dim(mxX)
@@ -152,7 +183,8 @@ dim(mxX)
 bhatcross <- solve( crossprod(mxX) + diag(1, ncol(mxX)) ) %*% ( t(mxX) %*% y )
 
 
-
+mxH1_0 <- f_basisNatSplines(x=x1_0, xknots=xknots)
+mxH2_0 <- f_basisNatSplines(x=x2_0, xknots=xknots)
 mxX0 <- mxH1_0[ , xxeg[ , "ndx1"] ] * mxH2_0[ , -1][ , xxeg[ , "ndx2"] ]
 
 yhatcross0 <- mxX0 %*% bhatcross
@@ -160,12 +192,12 @@ yhatcross0 <- mxX0 %*% bhatcross
 xrmse <- sqrt( mean( (y0 - yhatcross0)^2 ) )
 xrmse
 
-if(xbool.savePlots) { png(file.path("~", "Desktop", "wiggles_simfitGAM_finteract_3d.png"), width=1000, height=1000, pointsize=18) }
-par(mfrow=c(1, 1))
-#scatterplot3d( x1, x2, y, angle=30, xlab="x1", ylab="x2", zlab="y", cex.symbols=0.3) ### , type="l"
-xsp3 <- scatterplot3d( x1, x2, y, angle=30, xlab="x1", ylab="x2", zlab="y", cex.symbols=0.3, main=paste0("Interacted Spines w/some Ridge!  RMSE: ", round(xrmse, 4))) ### , type="l"
-xsp3$points3d(x=x1, y=x2, z=yhatcross, col="#FF0000")
-if(xbool.savePlots) { dev.off() }
+#if(xbool.savePlots) { png(file.path("~", "Desktop", "wiggles_simfitGAM_finteract_3d.png"), width=1000, height=1000, pointsize=18) }
+#par(mfrow=c(1, 1))
+##scatterplot3d( x1, x2, y, angle=30, xlab="x1", ylab="x2", zlab="y", cex.symbols=0.3) ### , type="l"
+#xsp3 <- scatterplot3d( x1, x2, y, angle=30, xlab="x1", ylab="x2", zlab="y", cex.symbols=0.3, main=paste0("Interacted Spines w/some Ridge!  RMSE: ", round(xrmse, 4))) ### , type="l"
+#xsp3$points3d(x=x1, y=x2, z=yhatcross, col="#FF0000")
+#if(xbool.savePlots) { dev.off() }
 
 
 
